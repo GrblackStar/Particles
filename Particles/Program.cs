0e6a839ad2a81bf5336f2b2d02a8d3e94ef2766b
HHristova <86851411+GrblackStar@users.noreply.github.com>
2025-01-05 13:34:43 +0200

    Initial Commit

---
+++ Particles/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Emotion.Common;
+using Emotion.Graphics;
+using Emotion.Primitives;
+using Emotion.Scenography;
+
+namespace Particles;
+
+public static class Program
+{
+    public static void Main()
+    {
+        Engine.Start(new Configurator()
+        {
+            DebugMode = true,
+            HostTitle = "Particles!"
+        }, StartRoutineAsync);
+    }
+
+    public static IEnumerator StartRoutineAsync()
+    {
+        yield return Engine.SceneManager.SetScene(new ParticlesTestScene());
+    }
+}
+
+public class ParticlesTestScene : SceneWithMap
+{
+    protected override IEnumerator InternalLoadSceneRoutineAsync()
+    {
+        yield break;
+    }
+
+    public override void UpdateScene(float dt)
+    {
+        base.UpdateScene(dt);
+
+
+    }
+
+    public override void RenderScene(RenderComposer c)
+    {
+        base.RenderScene(c);
+
+        c.RenderSprite(new System.Numerics.Vector3(0, 0, 0), new System.Numerics.Vector2(10, 10), Color.PrettyYellow);
+    }
+}
